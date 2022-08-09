61e9f03ff81610a69f48436ba560f253070f16ab
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-08-09 17:15:17 +0200

    Add draft list of words to test program workflow

---
+++ app/src/main/java/pl/glownia/maciej/unlockenglishword/ui/unlock/ListOfWordAndItsDefinition.kt
@@ -0,0 +1,13 @@
+package pl.glownia.maciej.unlockenglishword.ui.unlock
+
+// Map of english words as keys and their definitions as values
+val allWordsAndItsDefinitionList: Map<String, String> = mapOf(
+        "a" to "indefinite article",
+        "abandon" to "to leave somebody, especially somebody you are responsible for, with no intention of returning",
+        "ability" to "the fact that somebody/something is able to do something",
+        "able" to "to have the skill, intelligence, opportunity, etc. needed to do something",
+        "abortion" to "the deliberate ending of a pregnancy at an early stage",
+        "about" to "on the subject of somebody/something; in connection with",
+        "above" to "at or to a higher place or position than something/somebody",
+        "abroad" to "in or to a foreign country"
+)
