206c83ad7122d8aa1ac2771fa67e144b5a1584ca
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-08-09 17:07:34 +0200

    Set up MainActivity for FragmentContainerView

--- app/src/main/java/pl/glownia/maciej/unlockenglishword/MainActivity.kt
+++ app/src/main/java/pl/glownia/maciej/unlockenglishword/MainActivity.kt
@@ -3,6 +3,9 @@ package pl.glownia.maciej.unlockenglishword
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Creates an Activity that hosts the UnlockWordFragment in the app.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
